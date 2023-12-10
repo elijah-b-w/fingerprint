f2488d885fe27ff0a09ea72ed1281690b5fcfc56
Elijah <122858073+elijah-b-w@users.noreply.github.com>
2023-12-10 13:25:33 -0700

    Update fingerprintTest.cpp

--- fingerprintTest.cpp
+++ fingerprintTest.cpp
@@ -1,6 +1,8 @@
 /*
 Test file for fingerprint class
 Tests fingerprint matching
+
+sample prints from: http://bias.csr.unibo.it/fvc2000/db3.asp
 */
 
 #include <iostream>
@@ -12,45 +14,14 @@ Tests fingerprint matching
 // using namespace cv;
 
 int main() {
-    /*
-    // sample making sure opencv is working
-    std::string image_path = "images/101_1.tif";
-    cv::Mat img = imread(image_path, IMREAD_GRAYSCALE); // read in grayscale
-
-    // error handling
-    if (img.empty()) {
-        std::cout << "Image not found" << std::endl;
-
-        return -1;
-    }
-
-    cv::namedWindow("window");
-    cv::imshow("window", img);
-    int k = cv::waitKey(0); // wait for a keystroke to close
-    */
 
     // Test image 1
     const std::string imagePath = "images/101_1.tif";
     Fingerprint testPrint1 = Fingerprint(imagePath);
 
-    // show with mask
-    // testPrint1.setMask();
 
     // get keypoints
     testPrint1.setKeypoints();
-
-    //  add keypoints to image
-    testPrint1.displayKeypoints();
-
-    // testPrint1.displayPrint();
-
-    // Test image 1 copy
-    // const std::string image_path2 = "images/101_1.tif";
-    // Fingerprint testPrint2 = Fingerprint(image_path2);
-
-    // get keypoints
-    // testPrint2.setKeypoints();
-    // testPrint2.setKeypoints();
 
     // Test image 2 - should match 1
     const std::string imagePath2 = "images/101_3.tif";
@@ -59,16 +30,8 @@ int main() {
     testPrint2.setKeypoints();
 
     
-    // testPrint1.fingerMatches(testPrint1, testPrint2);
-    // testPrint1.displayMatches(testPrint1, testPrint2);
-
-
-    /*
-    // checks if prints are equal
-    if (testPrint1 == testPrint2) {
-        std::cout << "Success!" << std::endl;
-    }
-    */
+    testPrint1.fingerMatches(testPrint1, testPrint2);
+    
 
 
     return 0;
